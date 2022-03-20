6febc4de89e31a8f5359594b069951bcf9d5b490
Ray Louis <x1337c@gmail.com>
2022-03-20 11:50:55 -0700

    i'm confused

--- pages/api/get-banned-users.ts
+++ pages/api/get-banned-users.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { MongoClient } from "mongodb";
-const connectionString: string = process.env.ATLAS_URI || ""
-const client = new MongoClient(connectionString)
+// import { MongoClient } from "mongodb";
+// const connectionString: string = process.env.ATLAS_URI || ""
+// const client = new MongoClient(connectionString)
+import { getMongoDbConnection } from "../api/module-mongo-setup"
 
 type errorMessage = {
   message: string,
@@ -12,10 +13,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<errorMessage | usersList>
 ) {
-  // get data from your database, for real
   try {
-    await client.connect()
-    const database = client.db("bpl-all-users");
+    const database = await getMongoDbConnection()
     const unbannedUsers = database.collection('banned-users')
     const usersList = await unbannedUsers.find({}).limit(50).toArray()
     res.status(200).send(usersList)
@@ -23,6 +22,5 @@ export default async function handler(
     console.error("Something went wrong:", err)
     res.status(400).send({ message: String(err) })
   }
-
 }
 
--- pages/api/module-mongo-setup.ts
+++ pages/api/module-mongo-setup.ts
@@ -2,17 +2,18 @@ import { Db, MongoClient } from "mongodb";
 const connectionString: string = process.env.ATLAS_URI || ""
 const client = new MongoClient(connectionString)
 
-let db
+let db: Db 
 
-export default async function initiateMongoDbConnection() {
+export async function initiateMongoDbConnection(): Promise<void> {
   try {
     await client.connect()
     console.log("Connected to MongoDB Atlas.")
-    let db: Db = client.db("bpl-all-users");
-    return db
+    db = client.db("bpl-all-users");
   } catch (err) {
     console.error("Something went wrong:", err)
   }
 }
 
-export { db }
+export function getMongoDbConnection(): Db {
+  return db
+}
