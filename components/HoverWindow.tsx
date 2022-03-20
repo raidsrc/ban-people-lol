d10cb85eb16e0a76cb864e6d76aba28f22114cb0
Ray Louis <x1337c@gmail.com>
2022-03-20 14:46:47 -0700

    got the hoverwindow to animate out!

--- components/HoverWindow.tsx
+++ components/HoverWindow.tsx
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb"
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useState } from "react"
 import { KeyedMutator } from "swr"
 import type { userObjectType } from "../pages/types"
 
@@ -11,12 +11,14 @@ const HoverWindow = ({ userObject, bannedUsers, setShowSelf, mutate }: {
   setShowSelf: Dispatch<SetStateAction<boolean>>
   mutate: KeyedMutator<String>
 }) => {
+
   function handleBanButtonClick() {
     if (bannedUsers) unbanUser(userObject._id, mutate)
     else banUser(userObject._id, mutate)
     setShowSelf(false)
   }
   return (
+
     <span className="p-2 border-2 border-black w-80 absolute ml-6 -bottom-[5rem] bg-slate-100 bg-opacity-90 shadow-md">
       {/* <div className="w-4 h-4 top-12 -left-2 rotate-45 bg-inherit border-2 border-black absolute"> </div> */}
       <div>First name: <span className="capitalize">{userObject.fname}</span></div>
--- components/UserButtonComponent.tsx
+++ components/UserButtonComponent.tsx
@@ -3,8 +3,7 @@ import { ObjectId } from "mongodb"
 import HoverWindow from "./HoverWindow"
 import { useState } from "react"
 import { UserButtonComponentProps } from "../pages/types"
-
-
+import { CSSTransition } from "react-transition-group"
 
 const UserButtonComponent = ({ userObject, mutate, bannedUsers }: UserButtonComponentProps) => {
   const [showHoverWindow, setShowHoverWindow] = useState(false)
@@ -15,14 +14,16 @@ const UserButtonComponent = ({ userObject, mutate, bannedUsers }: UserButtonComp
         <div className="my-1 p-1 px-2 border-2 flex items-center min-w-max">
           <span className="pr-4">{userObject.username}</span>
           <button onClick={() => {
-            setShowHoverWindow((prev) => !prev)
+            setShowHoverWindow(prev => !prev)
           }} >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
               <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
             </svg>
           </button>
           <span className='relative'>
-            {showHoverWindow ? <HoverWindow userObject={userObject} bannedUsers={bannedUsers} setShowSelf={setShowSelf} mutate={mutate}/> : ""}
+            <CSSTransition in={showHoverWindow} timeout={200} classNames="hover-window" unmountOnExit>
+              <HoverWindow userObject={userObject} bannedUsers={bannedUsers} setShowSelf={setShowSelf} mutate={mutate} />
+            </CSSTransition>
           </span>
         </div> : ""}
     </div>
