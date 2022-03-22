4a5e85eba6aab5e2e507f81951f23d3c5387b2c4
Ray Louis <x1337c@gmail.com>
2022-03-21 23:31:31 -0700

    refactored index and _app with mui and custom typography

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/output.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from "@mui/material"
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
