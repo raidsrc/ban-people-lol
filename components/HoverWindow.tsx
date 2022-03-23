d6ac8c78421f9a570c96f40dc5bc9d7c4e66b5dd
Ray Louis <x1337c@gmail.com>
2022-03-23 11:12:06 -0700

    hoverwindow paper

--- components/HoverWindow.tsx
+++ components/HoverWindow.tsx
@@ -1,3 +1,4 @@
+import { Card, Paper } from "@mui/material"
 import type { userObjectType } from "../pages/_types"
 
 
@@ -5,15 +6,17 @@ const HoverWindow = ({ userObject }: {
   userObject: userObjectType
 }) => {
 
-  
+
   return (
-    <div className="p-2 border-2 border-black w-80 absolute bg-slate-100 bg-opacity-90 shadow-md z-10">
-      {/* <div className="w-4 h-4 top-12 -left-2 rotate-45 bg-inherit border-2 border-black absolute"> </div> */}
-      <div>First name: <span className="capitalize">{userObject.fname}</span></div>
-      <div>Last name: <span className="capitalize">{userObject.lname}</span></div>
-      <div>Number of times banned: {userObject["number-of-times-banned"]}</div>
-      <div>Date joined: {String(userObject["date-joined"]).split("T")[0]}</div>
-    </div>
+    <Paper elevation={3} variant="outlined" className="p-4 w-80 absolute bg-slate-100 bg-opacity-90 shadow-md z-10">
+      {/* <div className="p-2 border-2 border-black w-80 absolute bg-slate-100 bg-opacity-90 shadow-md z-10"> */}
+        {/* <div className="w-4 h-4 top-12 -left-2 rotate-45 bg-inherit border-2 border-black absolute"> </div> */}
+        <div>First name: <span className="capitalize">{userObject.fname}</span></div>
+        <div>Last name: <span className="capitalize">{userObject.lname}</span></div>
+        <div>Number of times banned: {userObject["number-of-times-banned"]}</div>
+        <div>Date joined: {String(userObject["date-joined"]).split("T")[0]}</div>
+      {/* </div> */}
+    </Paper>
   )
 }
 
