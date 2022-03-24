e848b84aeafe57facfab9f92f3e3938e4dc99d6a
Ray Louis <x1337c@gmail.com>
2022-03-23 22:09:38 -0700

    passHref

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
           >
             <List>
               <ListItem>
-                <Link href="/banned-users" >
+                <Link href="/banned-users" passHref >
                   <ListItemButton>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
                       <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
                 </Link>
               </ListItem>
               <ListItem>
-                <Link href="/unbanned-users" >
+                <Link href="/unbanned-users" passHref >
                   <ListItemButton>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
                       <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
